1f4c5cbb78a0da6486549a7c989e03451cffa0e8
abcdefak87 <abcdefak87@gmail.com>
2026-01-09 01:18:39 +0700

    fix: index.html and deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,18 +4,15 @@
 
 set -e
 
-# Pull latest
 git reset --hard
 git pull
 
-# Generate version string (YYYYMMDDHHMMSS)
 VERSION=$(date +%Y%m%d%H%M%S)
 
-# Copy APK with new name
+# Copy APK
 cp deploy/tulongen-latest.apk /var/www/tulongen/apk/tulongen.apk
 
-# Update index.html with new version to bypass Cloudflare cache
-sed "s/?v=[0-9]*/?v=$VERSION/g" deploy/index.html > /var/www/tulongen/index.html
+# Update version in index.html and copy
+sed "s/tulongen\.apk?v=[0-9]*/tulongen.apk?v=$VERSION/g" deploy/index.html > /var/www/tulongen/index.html
 
-echo "Done! Version: $VERSION"
-echo "URL: https://tulongen.raf.my.id/apk/tulongen.apk?v=$VERSION"
+echo "Done! v=$VERSION"
