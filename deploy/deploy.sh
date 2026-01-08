d6e0ab2efd7798cd8eb1d1b39eadae70bc893658
abcdefak87 <abcdefak87@gmail.com>
2026-01-09 01:15:21 +0700

    fix: cache busting with version query param

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,8 +2,20 @@
 # Tulongen Deploy Script
 # Usage: cd ~/tulongen.apk && bash deploy/deploy.sh
 
+set -e
+
+# Pull latest
 git reset --hard
 git pull
-cp deploy/tulongen-latest.apk /var/www/tulongen/apk/
-cp deploy/index.html /var/www/tulongen/
-echo "Done!"
+
+# Generate version string (YYYYMMDDHHMMSS)
+VERSION=$(date +%Y%m%d%H%M%S)
+
+# Copy APK with new name
+cp deploy/tulongen-latest.apk /var/www/tulongen/apk/tulongen.apk
+
+# Update index.html with new version to bypass Cloudflare cache
+sed "s/?v=[0-9]*/?v=$VERSION/g" deploy/index.html > /var/www/tulongen/index.html
+
+echo "Done! Version: $VERSION"
+echo "URL: https://tulongen.raf.my.id/apk/tulongen.apk?v=$VERSION"
